1b8590613fbd86aeeb5aee641f8d59c3c3ea9c12
Jorge Vazquez <jorgevs.mx@gmail.com>
2018-10-14 11:11:33 -0400

    Cleaning code

--- src/main/java/com/jvs/springboot/SpringBootStandaloneApp/SpringBootStandaloneAppApplication.java
+++ src/main/java/com/jvs/springboot/SpringBootStandaloneApp/SpringBootStandaloneAppApplication.java
@@ -1,11 +1,9 @@
 package com.jvs.springboot.SpringBootStandaloneApp;
 
-import com.jvs.springboot.SpringBootStandaloneApp.com.jvs.springboot.service.DefaultHelloService;
 import com.jvs.springboot.SpringBootStandaloneApp.com.jvs.springboot.service.HelloService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import javax.annotation.Resource;
 
@@ -15,15 +13,10 @@ public class SpringBootStandaloneAppApplication implements CommandLineRunner {
     @Resource
     private HelloService helloService;
 
-    @Bean
-    public HelloService getHelloService(){
-        return  new DefaultHelloService();
-    }
-
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Spring Boot - Standalone App");
-        getHelloService().hello();
+        helloService.hello();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/jvs/springboot/SpringBootStandaloneApp/com/jvs/springboot/service/DefaultHelloService.java
+++ src/main/java/com/jvs/springboot/SpringBootStandaloneApp/com/jvs/springboot/service/DefaultHelloService.java
@@ -1,8 +1,13 @@
 package com.jvs.springboot.SpringBootStandaloneApp.com.jvs.springboot.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class DefaultHelloService implements HelloService {
+
     @Override
     public void hello() {
         System.out.println("Hello world!... from Hello Service");
     }
+
 }
